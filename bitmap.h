fce8fd2d47df9e3e48ddd1a4da4ef3537385bbb7
German <german.fidarov@gmail.com>
2022-04-18 14:43:43 +0300

    Fix seek position

--- bitmap.h
+++ bitmap.h
@@ -7,6 +7,8 @@ short checkBMPFile(FILE* f) // check if file is a bmp one
     uint8_t header[2]; // .bmp file header (aka 'BM')
     int result = fread(&header, 2, 1, f);
 
+    fseek(f, 0, SEEK_SET);
+
     if (header[0] == 66 && header[1] == 77)
     {
         return 0;
