ac11916a7d11fff548e3a4a7102b76932e62770b
German <german.fidarov@gmail.com>
2022-04-19 02:36:43 +0300

    Fix crash on file loading

--- bitmap.h
+++ bitmap.h
@@ -2,62 +2,69 @@
 #define DWORD uint32_t
 #define LONG uint32_t
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#pragma pack(push, 1)
+
 typedef struct tagBITMAPFILEHEADER
 {
-    WORD bfType;  //specifies the file type
-    DWORD bfSize;  //specifies the size in bytes of the bitmap file
-    WORD bfReserved1;  //reserved; must be 0
-    WORD bfReserved2;  //reserved; must be 0
-    DWORD bfOffBits;  //specifies the offset in bytes from the bitmapfileheader to the bitmap bits
-}BITMAPFILEHEADER;
+    WORD bfType;      // specifies the file type
+    DWORD bfSize;     // specifies the size in bytes of the bitmap file
+    WORD bfReserved1; // reserved; must be 0
+    WORD bfReserved2; // reserved; must be 0
+    DWORD bfOffBits;  // specifies the offset in bytes from the bitmapfileheader to the bitmap bits
+} BITMAPFILEHEADER;
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
 
 typedef struct tagBITMAPINFOHEADER
 {
-    DWORD biSize;  //specifies the number of bytes required by the struct
-    LONG biWidth;  //specifies width in pixels
-    LONG biHeight;  //specifies height in pixels
-    WORD biPlanes;  //specifies the number of color planes, must be 1
-    WORD biBitCount;  //specifies the number of bits per pixel
-    DWORD biCompression;  //specifies the type of compression
-    DWORD biSizeImage;  //size of image in bytes
-    LONG biXPelsPerMeter;  //number of pixels per meter in x axis
-    LONG biYPelsPerMeter;  //number of pixels per meter in y axis
-    DWORD biClrUsed;  //number of colors used by the bitmap
-    DWORD biClrImportant;  //number of colors that are important
-}BITMAPINFOHEADER;
+    DWORD biSize;         // specifies the number of bytes required by the struct
+    LONG biWidth;         // specifies width in pixels
+    LONG biHeight;        // specifies height in pixels
+    WORD biPlanes;        // specifies the number of color planes, must be 1
+    WORD biBitCount;      // specifies the number of bits per pixel
+    DWORD biCompression;  // specifies the type of compression
+    DWORD biSizeImage;    // size of image in bytes
+    LONG biXPelsPerMeter; // number of pixels per meter in x axis
+    LONG biYPelsPerMeter; // number of pixels per meter in y axis
+    DWORD biClrUsed;      // number of colors used by the bitmap
+    DWORD biClrImportant; // number of colors that are important
+} BITMAPINFOHEADER;
+
+#pragma pack(pop)
 
 unsigned char *LoadBitmapFile(FILE *filePtr, BITMAPINFOHEADER *bitmapInfoHeader, BITMAPFILEHEADER *bitmapFileHeader)
 {
     // BITMAPFILEHEADER bitmapFileHeader;  //our bitmap file header
-    unsigned char *bitmapImage;  //store image data
-    int imageIdx=0;  //image index counter
-    unsigned char tempRGB;  //our swap variable
+    unsigned char *bitmapImage; // store image data
+    int imageIdx = 0;           // image index counter
+    unsigned char tempRGB;      // our swap variable
 
-    //read the bitmap file header
-    fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER),1,filePtr);
+    // read the bitmap file header
+    fread(bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
 
-    //verify that this is a .BMP file by checking bitmap id
-    if (bitmapFileHeader->bfType !=0x4D42)
+    // verify that this is a .BMP file by checking bitmap id
+    if (bitmapFileHeader->bfType != 0x4D42)
     {
         fclose(filePtr);
         return NULL;
     }
 
-    //read the bitmap info header
-    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER),1,filePtr); 
+    // read the bitmap info header
+    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
 
-    //move file pointer to the beginning of bitmap data
+    // move file pointer to the beginning of bitmap data
     fseek(filePtr, bitmapFileHeader->bfOffBits, SEEK_SET);
 
-    //allocate enough memory for the bitmap image data
-    bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
+    // allocate enough memory for the bitmap image data
+    bitmapImage = (unsigned char *)malloc(bitmapInfoHeader->biSizeImage);
 
-    //verify memory allocation
+    // verify memory allocation
     if (!bitmapImage)
     {
         free(bitmapImage);
@@ -65,25 +72,25 @@ unsigned char *LoadBitmapFile(FILE *filePtr, BITMAPINFOHEADER *bitmapInfoHeader,
         return NULL;
     }
 
-    //read in the bitmap image data
-    fread(bitmapImage,bitmapInfoHeader->biSizeImage,1,filePtr);
+    // read in the bitmap image data
+    fread(bitmapImage, bitmapInfoHeader->biSizeImage, 1, filePtr);
 
-    //make sure bitmap image data was read
+    // make sure bitmap image data was read
     if (bitmapImage == NULL)
     {
         fclose(filePtr);
         return NULL;
     }
 
-    //swap the R and B values to get RGB (bitmap is BGR)
-    for (imageIdx = 0;imageIdx < bitmapInfoHeader->biSizeImage;imageIdx+=3)
+    // swap the R and B values to get RGB (bitmap is BGR)
+    for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx += 3)
     {
         tempRGB = bitmapImage[imageIdx];
         bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
         bitmapImage[imageIdx + 2] = tempRGB;
     }
 
-    //close file and return bitmap image data
+    // close file and return bitmap image data
     fclose(filePtr);
     return bitmapImage;
 }
--- coder.c
+++ coder.c
@@ -3,33 +3,45 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        printWrongUsage();
-        exit(EXIT_FAILURE);
-    }
+    // if (argc != 4)
+    // {
+    //     printWrongUsage();
+    //     exit(EXIT_FAILURE);
+    // }
 
-    FILE* containerFile = fopen(argv[1], "rw");
-    FILE* inputFile = fopen(argv[2], "r");
+    // FILE *containerFile = fopen(argv[1], "rb");
+    // FILE *inputFile = fopen(argv[2], "rb");
 
-    unsigned int containerSize;
+    FILE *containerFile = fopen("stormrooper.bmp", "rb");
+    FILE *inputFile = fopen("image.bmp", "rb");
 
-    if(containerFile && inputFile)
-    {
-        if(checkBMPFile(containerFile))
-        {
-            printWrongFiles();
-            return 0;
-        }
-        
-        containerSize = calculateFileSize(containerFile);
-    }
 
-    else
+    if (containerFile == NULL || inputFile == NULL)
     {
         printWrongFiles();
-        exit(EXIT_FAILURE);
+        return 0;
     }
+
+    BITMAPFILEHEADER containerBitmapFileHeader;
+    BITMAPFILEHEADER inputBitmapFileHeader;
+
+    BITMAPINFOHEADER containerBitmapInfoHeader;
+    BITMAPINFOHEADER inputBitmapInfoHeader;
+
+    unsigned char *containerBitmapData;
+    unsigned char *inputBitmapData;
+
+    containerBitmapData = LoadBitmapFile(containerFile,
+                                         &containerBitmapInfoHeader,
+                                         &containerBitmapFileHeader);
+
+    inputBitmapData = LoadBitmapFile(inputFile,
+                                     &inputBitmapInfoHeader,
+                                     &inputBitmapFileHeader);
+
+    printf("input file size: %d\ncontainer file size: %d\n",
+           inputBitmapFileHeader.bfSize,
+           containerBitmapFileHeader.bfSize);
 
     return 0;
 }
